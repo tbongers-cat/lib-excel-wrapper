6a7af1c57748ec38b5f2c90f0eb5e221ac9f3217
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2018-05-08 16:34:56 +0200

    Use namespace in test

--- tests/StyleTest.php
+++ tests/StyleTest.php
@@ -2,8 +2,9 @@
 
 require_once(__DIR__."/../src/Style.php");
 use \CaT\Libs\ExcelWrapper\Style;
+use PHPUnit\Framework\TestCase;
 
-class StyleTest  extends PHPUnit_Framework_TestCase {
+class StyleTest  extends TestCase {
 	public function setUp() {
 		$this->style = new Style();
 	}
